e704b79d00c637e0b540e4f6dc6b90e89b60eb26
Colin Woodbury <colingw@gmail.com>
2017-08-01 15:28:12 -0700

    Carry command-line `Args` directly in runtime `Env`

--- src/Main.hs
+++ src/Main.hs
@@ -22,31 +22,25 @@ import           Shelly
 
 ---
 
-data Args = Args { areas :: String <?> "Path to a JSON file defining areas to convert."
-                 , cache :: FilePath <?> "Path to a directory for storing the downloaded .pbf files."
-                 , threads :: Int <?> "Number of CPU threads to use."
+-- | Command-line arguments.
+data Args = Args { areas   :: String   <?> "Path to a JSON file defining areas to convert."
+                 , cache   :: FilePath <?> "Path to a directory for storing the downloaded .pbf files."
+                 , osm2orc :: FilePath <?> "Path to an osm2orc executable."
+                 , s3      :: Text     <?> "S3 Bucket/Key to upload to, like: s3://vectortiles/orc/"
+                 , threads :: Int      <?> "Number of CPU threads to use."
                  } deriving (Generic, ParseRecord)
 
 -- | The runtime environment.
-data Env = Env { _cache :: FilePath
-               , _threads :: Int
-               , _downloaded :: M.Map Text FilePath
-               , _countries :: TChan (Text, Text)
-               , _toConvert :: TChan FilePath
-               , _toUpload :: TChan Text
-               , _downloadDone :: TVar Int
+data Env = Env { _args           :: Args
+               , _downloaded     :: M.Map Text FilePath
+               , _countries      :: TChan (Text, Text)
+               , _toConvert      :: TChan FilePath
+               , _toUpload       :: TChan Text
+               , _downloadDone   :: TVar Int
                , _conversionDone :: TVar Int }
 
 -- | An OSM Extract area to process.
 data Area = Area { name :: Text, countries :: [Text] } deriving (Generic, FromJSON)
-
--- | Location of `osm2orc` executable (java program).
-osm2orc :: FilePath
-osm2orc = "/home/colin/code/java/osm2orc/build/distributions/osm2orc-0.3.1/bin/osm2orc"
-
--- | Where to upload the converted `.orc` files to.
-s3 :: Text
-s3 = "s3://vectortiles/orc/"
 
 -- | Given an `Area` name and a country, form the URL of where its PBF file should be located.
 url :: Text -> Text -> Text
@@ -62,7 +56,7 @@ download env = do
   where getNext n c = case M.lookup c (_downloaded env) of
           Just fp -> TIO.putStrLn (c <> " already downloaded.") >> pure fp
           Nothing -> do
-            let fp = _cache env </> c <.> "osm.pbf"
+            let fp = (unHelpful . cache $ _args env) </> c <.> "osm.pbf"
             TIO.putStrLn $ "Downloading " <> c <> " data..."
             shelly $ run_ "wget" ["-q", "-O", toTextIgnore fp, url n c]
             pure fp
@@ -71,13 +65,13 @@ convert :: Env -> IO ()
 convert env = do
   next <- atomically $ (,) <$> readTVar (_downloadDone env) <*> tryReadTChan (_toConvert env)
   case next of
-    (n, Nothing) | n == _threads env -> atomically $ modifyTVar' (_conversionDone env) (+ 1)
+    (n, Nothing) | n == (unHelpful . threads $ _args env) -> atomically $ modifyTVar' (_conversionDone env) (+ 1)
                  | otherwise -> threadDelay 1000000 >> convert env
     (_, Just pbf) -> do
       let pbf' = toTextIgnore pbf
-          orc  = toTextIgnore $ _cache env </> basename pbf <.> "orc"
+          orc  = toTextIgnore $ (unHelpful . cache $ _args env) </> basename pbf <.> "orc"
       TIO.putStrLn $ "Converting " <> pbf'
-      shelly $ run_ osm2orc [pbf', orc]
+      shelly $ run_ (unHelpful . osm2orc $ _args env) [pbf', orc]
       atomically $ writeTChan (_toUpload env) orc
       convert env
 
@@ -86,31 +80,29 @@ upload :: Env -> IO ()
 upload env = do
   next <- atomically $ (,) <$> readTVar (_conversionDone env) <*> tryReadTChan (_toUpload env)
   case next of
-    (n, Nothing) | n == _threads env -> pure ()  -- All uploads complete.
+    (n, Nothing) | n == (unHelpful . threads $ _args env) -> pure ()  -- All uploads complete.
                  | otherwise -> threadDelay 1000000 >> upload env
     (_, Just orc) -> do
       TIO.putStrLn $ "Uploading " <> orc
-      shelly $ run_ "aws" ["s3", "cp", orc, s3, "--quiet"]
+      shelly $ run_ "aws" ["s3", "cp", orc, unHelpful . s3 $ _args env, "--quiet"]
       TIO.putStrLn $ "Uploading " <> orc <> " complete!"
       upload env
 
-work :: Env -> Int -> [Area] -> IO ()
-work env t as = do
+work :: Env -> [Area] -> IO ()
+work env as = do
   atomically . mapM_ (writeTChan (_countries env)) $ as >>= \(Area n cs) -> map (\c -> (n, c)) cs
-  void . runConcurrently $ (,,)
-    <$> Concurrently (replicateConcurrently_ t $ download env)
-    <*> Concurrently (replicateConcurrently_ t $ convert env)
-    <*> Concurrently (replicateConcurrently_ t $ upload env)
+  void . runConcurrently $ (,,) <$> f download <*> f convert <*> f upload
+  where f g = Concurrently . replicateConcurrently_ (unHelpful . threads $ _args env) $ g env
 
 main :: IO ()
 main = do
-  Args (Helpful as) (Helpful c) (Helpful t) <- getRecord "Draenor - Batch convert OSM PBF to ORC and upload to S3."
-  pbfs <- shelly $ mkdir_p c >> ls c  -- All already downloaded pbf files.
-  bytes <- B.readFile as
+  args <- getRecord ".osm.pbf -> .orc, and upload to S3."
+  pbfs <- shelly $ mkdir_p (unHelpful $ cache args) >> ls (unHelpful $ cache args)  -- Already downloaded pbf files.
+  bytes <- B.readFile . unHelpful $ areas args
   (cc, fc, tc) <- (,,) <$> newTChanIO <*> newTChanIO <*> newTChanIO
   (dt, ct) <- (,) <$> newTVarIO 0 <*> newTVarIO 0
   let pbfs' = M.fromList . map (\p -> (toTextIgnore $ basename p, p)) $ filter (hasExt "pbf") pbfs
-      env = Env c t pbfs' cc fc tc dt ct
+      env = Env args pbfs' cc fc tc dt ct
   case decode bytes of
     Nothing -> putStrLn "There was a parsing error in your JSON file."
-    Just as' -> work env t as' >> putStrLn "Done."
+    Just as' -> work env as' >> putStrLn "Done."
